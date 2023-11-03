30ef72f05fc1f6b6b40aaff488cef6d3f9c18cce
Guilherme <guycanella@gmail.com>
2023-11-03 19:12:34 -0300

    update dynamicshelf

--- components/product/DynamicShelf.tsx
+++ components/product/DynamicShelf.tsx
@@ -32,7 +32,72 @@ interface VariantSeason {
   type: 'estacao'
   /**
    * @title Estação
+   */import type { Product } from 'apps/commerce/types.ts'
+
+import ClimateShelf from "../ui/ProductShelfClimate.tsx";
+import { ProductShelfProps } from "$store/components/product/ProductShelf.tsx";
+
+
+interface VariantDatetime {
+  type: 'data'
+  /**
+   * @title Start range at date
+   * @format datetime
    */
+  variantStart: string
+  /**
+   * @title End range at date
+   * @format datetime
+   */
+  variantEnd: string
+  products: Product[] | null
+}
+
+interface VariantClimate {
+  type: 'clima'
+  /**
+   * @title Temperature
+   */
+  temperature: number
+  /**
+   * @title Shelf specific
+   */
+  shelfToRender: Product[] | null
+  /**
+   * @title Shelf default
+   */
+  products: Product[] | null
+}
+
+interface VariantSeason {
+  type: 'estacao'
+  /**
+   * @title Estação
+   */
+  variantActive: 'verão' | 'inverno'
+  products: Product[] | null
+}
+
+export interface DynamicShelfProps extends Omit<ProductShelfProps, 'products'> {
+  variations: VariantDatetime | VariantClimate | VariantSeason
+}
+
+export default function DynamicShelf({ variations, ...restOfProps }: DynamicShelfProps) {
+
+  if ("temperature" in variations) {
+    return (
+      <ClimateShelf
+        defaultShelf={variations.products}
+        shelfToRender={variations.shelfToRender}
+        temperature={variations.temperature}
+        {...restOfProps}
+      />
+    )
+  }
+
+  return <div>teste</div>
+}
+
   variantActive: 'verão' | 'inverno'
   products: Product[] | null
 }
