df1ed36d88caca6e7d6a584a8ad2c29c2ab42901
ErikIeto <erik.ieto@corebiz.ag>
2023-11-03 18:52:05 -0300

    feat(Season type): removing unused code

--- components/product/SeasonType.tsx
+++ components/product/SeasonType.tsx
@@ -24,62 +24,6 @@ interface VariationProps {
 export const SeasonType = (variations: VariationProps) => {
   const currentDate = new Date();
 
-  // Seasons date
-  const autumnSeason = new Date(currentDate.getFullYear(), 2, 21);
-  const winterSeason = new Date(currentDate.getFullYear(), 5, 21);
-  const springSeason = new Date(currentDate.getFullYear(), 8, 23);
-  const summerSeason = new Date(currentDate.getFullYear(), 11, 21);
-
-  // Check seasons
-  function checkSeason(date: Date) {
-    if (date >= autumnSeason && date < winterSeason) {
-      return "outono";
-    } else if (date >= winterSeason && date < springSeason) {
-      return "inverno";
-    } else if (date >= springSeason && date < summerSeason) {
-      return "primavera";
-    } else {
-      return "verão";
-    }
-  }
-
-  // Check range
-  function checkRange(date: Date, start: Date, end: Date) {
-    return date >= start && date < end;
-  }
-
-  // Find seasons
-  function findSeason(date: Date, seasonName: string, start: Date, end: Date) {
-    const season = checkSeason(date);
-    const isSeason = season === seasonName;
-    const isRange = checkRange(date, start, end);
-    return isSeason && isRange;
-  }
-
-  if (findSeason(currentDate, "outono", autumnSeason, winterSeason)) {
-    console.log("Estamos no outono");
-  } else {
-    console.log("Não estamos no outono, fora do intervalo");
-  }
-
-  if (findSeason(currentDate, "inverno", winterSeason, springSeason)) {
-    console.log("Estamos no inverno");
-  } else {
-    console.log("Não estamos no inverno, fora do intervalo");
-  }
-
-  if (findSeason(currentDate, "primavera", springSeason, summerSeason)) {
-    console.log("Estamos no primavera");
-  } else {
-    console.log("Não estamos no primavera, fora do intervalo");
-  }
-
-  if (findSeason(currentDate, "verão", summerSeason, autumnSeason)) {
-    console.log("Estamos no verão");
-  } else {
-    console.log("Não estamos no verão, fora do intervalo");
-  }
-
   const getSeasonVariant: string[] = variations.variations.variations.map(
     (item: ProductProps) => item.variantActive,
   );
